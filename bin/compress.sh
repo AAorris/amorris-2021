0069e9016118598ec36287d1f425971152457405
Aaron Morris <aaorris@live.com>
2020-02-04 19:29:25 -0800

    Update homepage. Add images. Uncenter them.

--- bin/compress.sh
+++ bin/compress.sh
@@ -3,5 +3,5 @@ cd img
 rm -f p10.*
 for f in *.jpg
 do
-	convert $f -resize 10% -ordered-dither o8x8,1 -dither Floyd-Steinberg -comment "" -strip p10.$f
+	convert $f -resize 540x350 -ordered-dither o8x8,1 -dither Floyd-Steinberg -comment "" -strip p10.$f
 done
