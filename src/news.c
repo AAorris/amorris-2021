e69300f15eac7cd4c091f509fb91e381e6b6c2a1
Aaron Morris <aaorris@live.com>
2020-02-12 22:10:06 -0800

    Update makefile, news page. Add two images

--- src/news.c
+++ src/news.c
@@ -3,7 +3,7 @@
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
 	printf(
-		"<p><strong>%s</strong> - %s<br/><a href=\"%s\">%s</a><br/><sub>%s</sub>",
+		"<div class=\"card\"><h3>%s</h3><p>%s</p><p><a href=\"%s\">%s</a></p><p>#%s</p></div>",
 		argv[0], argv[1], argv[2], argv[2], argv[3]
 	);
 	return 0;
@@ -20,7 +20,7 @@ int main(int argc, char **argv) {
 	if (sqlite3_open(argv[1], &db)) {
 		fprintf(stderr, "Can't open db: %s\n", sqlite3_errmsg(db));
 	}
-	char* arg = "SELECT title, summary, url, (SELECT group_concat(tag, \" \") FROM tags WHERE tags.name = news.title) FROM news";
+	char* arg = "SELECT title, summary, url, (SELECT group_concat(tag, \" #\") FROM tags WHERE tags.name = news.title) FROM news";
 	if (sqlite3_exec(db, arg, callback, 0, &zErrMsg) != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
