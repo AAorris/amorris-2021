74aed0cb105afeb1c7d4ba853eea2d4e008d7b9b
Aaron Morris <aaorris@live.com>
2020-04-26 17:00:34 -0700

    Remove c code. Bring on the jamstack.

--- compress.sh
+++
@@ -1,8 +0,0 @@
-convert aaron-100.jpg -resize 10% -ordered-dither o8x8,1 -dither Floyd-Steinberg -comment "" -strip public/aaron-10.jpg
-cd img
-rm -f p10.*
-for f in *.jpg
-do
-	convert $f -resize 600x350^ -gravity Center -extent 600x350 -comment "" -strip p10.$f
-	# convert $f -resize 540x350 -ordered-dither o8x8,1 -dither Floyd-Steinberg -comment "" -strip p10.$f
-done
