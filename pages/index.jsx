eeac4e1af04e66505adf28f95d3d5e106be1900c
Aaron Morris <aaorris@live.com>
2020-05-02 07:39:37 -0700

    Add a header and social links.

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,27 +4,36 @@ import { Fragment } from "react";
 
 function Tags({ children }) {
     if (!children || !children.length) return null
-    return <sub className={"block"}>
-        🏷️ {children.map(tag => <span key={tag}  style={{padding: 4}}>{tag}</span>)}
+    return <sub className={"block-when-small float-right"}>
+        {children.map(tag => <span key={tag} className="tag">{tag}</span>)} 🏷️
     </sub>
 }
 
+function Brand(props) {
+    return <div className="brand" {...props} />
+}
+
 function Links({ items }) {
-    return <main style={{padding: "2em"}}>
+    return <main>
         <Head>
             <title>Links | Aaron Morris </title>
+            <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet"></link>
         </Head>
-        <h1>🏠 Aaron's Links</h1>
-        <sub className={"block"}>My brain at a glance</sub>
-        <img src="/img/p10.008.jpg" style={{width: 400}} />
-        <ul style={{listStyle: 'none', padding: 0}}>
+        <div style={{padding: 80, background: 'white', textAlign: 'center'}}>
+            <h1>Aaron Morris</h1>
+            <sub className={"block header"}>My 🧠 at a 🔎</sub>
+        </div>
+        {/* <img src="/img/p10.008.jpg" style={{width: 400}} />  */}
+        <ul style={{listStyle: 'none', padding: '0 2em'}}>
             {items.map(post => (
                 <li key={post.uid}>
-                    🔗 <a href={post.arg}>
+                    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                    <span>🔗 <a href={post.arg}>
                         <b> {post.title} </b>
                         <sub className={"block-when-small"}>{post.subtitle}</sub>
-                    </a>
-                    <Tags>{post.tags}</Tags>
+                    </a></span>
+                        <Tags>{post.tags || []}</Tags>
+                    </div>
                 </li>
             ))} 
         </ul>
@@ -57,4 +66,16 @@ export async function getStaticProps() {
         }).then(x => ({ props: { items }}))
 }
 
-export default Links
+export default (props) => (
+    <Fragment>
+        <header>
+            <Brand>Aaron Morris</Brand>
+            <div className="social" style={{display: 'flex'}}>
+                <a title="Updates on Twitter" href="https://twitter.com/aaorris">🐦</a>
+                <a title="Code on Github" href="https://github.com/aaorris">🐙</a>
+                <a title="Part of a Web Ring" href="https://webring.xxiivv.com ">💫</a>
+            </div>
+        </header>
+        <Links {...props} />
+    </Fragment>
+)
