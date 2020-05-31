f093d0a5218554a91c309acb5d55ac33e0b5910d
Aaron Morris <aaorris@live.com>
2020-05-30 21:20:45 -0700

    Clean up home page.

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ function Tags({ children }) {
     if (!children || !children.length) return null
     return <sub className={"block-when-small float-right"}>
         {children.map(tag => <span key={tag} className="tag">{tag}</span>)} 🏷️
-    </sub>
+    </sub>  
 }
 
 function Brand(props) {
@@ -70,22 +70,14 @@ function Links({ items }) {
         </div>
         <Block>
             <h2>My Goals</h2>
-            <p>I want to follow you and hear about what you think is interesting.</p>
-            <p>
-                <a href="https://twitter.com/intent/tweet?screen_name=aaorris&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">Tweet to @aaorris</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-            </p>
-            <p>I want to organize information to learn more about myself.</p>
-            <p>I want to achieve optimum general performance as a developer and partner.</p>
+            <p>I want to subscribe to updates across humanist and technotopian domains.</p>
+            <p>I want to organize information and learn more about myself in the process.</p>
+            <p>I want to achieve optimum brain health.</p>
             <p>I want to start a family and be a great dad.</p>
         </Block>
         <Block>
-            <h2>Say Hello</h2>
-            <div style={{display: 'grid', gridTemplateColumns: 'auto auto auto', alignItems: 'center', gridGap: '2rem'}}>
-                <h3>Twitter</h3>
-                <a href="https://twitter.com/aaorris?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @aaorris</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                <p>Twitter is where I post frequent tagged links.</p>
-            </div>
             <h2>See My Link Collection</h2>
+
             <div>{(([js, web, tech, home, life, science, graphics, math, design, enterprise, news]) => (
                 <Fragment>
                     <p>I share information on social media and collect it here.</p>
@@ -93,6 +85,7 @@ function Links({ items }) {
                     <p>Are you a designer? I share resources like {design} and {graphics}.</p>
                     <p>Are you thoughtful about life and learning? Try {home} {life} {science} and {math}.</p>
                     <p>Are you business focused? Check out my {enterprise} {news} links.</p>
+                    <p>Or just see everything at <Link href="/links"><a className="underline">Links</a></Link></p>
                 </Fragment>
             ))(['js', 'web', 'tech', 'home', 'life', 'science', 'graphics', 'math', 'design', 'enterprise', 'news'].map(tag => (
                <Link href={"/links/tagged/[tag]"} as={`/links/tagged/${tag}`}>
@@ -110,17 +103,9 @@ function Links({ items }) {
         <Block>
             <h2>About Me</h2>
             <img style={{display: 'block', margin: 'auto', padding: 20}} src="/img/p10.010.jpg" /> 
-            <p>This is me! I'm 26 years old and I'm spending every day learning, growing, and taking action.</p>
-            <p>Most people choose to tackle single-minded goals.
-                My single-minded goal is brain-care. My top brain care tip is to grow your left and right brain together.</p>
-            <p>My goal is to become an authority on how you can accelerate your brain's performance,
-                so you can learn and recall faster, remember important moments and information, and solve problems better
-                than you ever thought possible using meta-learning techniques and augmentation with technology.
-            </p>
-            <p>I hope you'll follow me as I journey towards my goals. For now, you can come here to see all the
-                links I think are interesting. I save them so I can look back on them, and I hope you'll enjoy
-                what I'm building.
-            </p>
+            <p>I'm a full-stack engineer at Zapier. We're working to democratize automation.</p>
+            <p>My personal goal is practicing meta-learning. That means learning about learning itself, and how to do that better.</p>
+            <p>Follow me if you're interested via the website header! See you around.</p>
         </Block>
     </main>
 }
