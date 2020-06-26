f632d07823ba66d43e252cf45cbf30118f15989c
Aaron Morris <aaorris@live.com>
2020-06-26 08:14:51 -0700

    Reduce s-maxage

--- pages/api/links/index.js
+++ pages/api/links/index.js
@@ -3,6 +3,6 @@ import LinkService from '../../../services/links'
 export default async (req, res) => {
   const service = new LinkService()
   const links = await service.getAllLinks()
-  res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate, public')
+  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate, public')
   res.status(200).json({ links })
 }
--- pages/api/links/tagged/[tag].jsx
+++ pages/api/links/tagged/[tag].jsx
@@ -3,6 +3,6 @@ import LinkService from '../../../../services/links'
 export default async (req, res) => {
   const service = new LinkService()
   const result = await service.getLinksforTag(req.query.tag)
-  res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate, public')
+  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate, public')
   res.status(200).json(result)
 }
