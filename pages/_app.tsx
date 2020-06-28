c05cc18af873ce8faa9d079bbfd5dfa67227b224
Aaron Morris <aaorris@live.com>
2020-06-28 16:58:44 -0700

    Set stale while revalidate age as a number. Update code block for mobile. Reduce size of nav typography.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,15 +6,6 @@ import { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Fragment>
-    <Head>
-      <script dangerouslySetInnerHTML={{__html:`
-        /* segment */
-        !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var t=analytics.methods[e];analytics[t]=analytics.factory(t)}analytics.load=function(e,t){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+e+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=t};analytics.SNIPPET_VERSION="4.1.0";
-        analytics.load("NQOoVY7vqMs4QmycJouRzoiDKuuuTO0h");
-        analytics.page();
-        }}();
-      `}} />
-    </Head>
     <header>
     <a href="/" className="plain"><div className="brand">Aaron Morris</div></a>
         <div className="social" style={{display: 'flex'}}>
--- pages/api/notes/[slug].js
+++ pages/api/notes/[slug].js
@@ -3,6 +3,6 @@ import NotesService from '../../../services/notes'
 export default async (req, res) => {
   const service = new NotesService()
   const note = await service.getNote(req.query.slug)
-  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate, public')
+  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate=3600, public')
   res.status(200).json({note})
 }
