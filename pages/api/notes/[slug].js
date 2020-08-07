b4667f6aaa0933e65b36187fb56bd6d7e7c5a158
Aaron Morris <aaorris@live.com>
2020-08-07 09:38:50 -0700

    Add charset

--- pages/api/notes/[slug].js
+++ pages/api/notes/[slug].js
@@ -6,7 +6,7 @@ export default async function getNote(req, res) {
   });
   const body = note.body;
   res.writeHead(200, "OK", {
-    "Content-Type": "text/plain",
+    "Content-Type": "text/plain; charset=utf-8",
     "Cache-Control":
       "max-age=36000, s-maxage=3600, public, stale-while-revalidate",
   });
